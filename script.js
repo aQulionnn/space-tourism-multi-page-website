ab2b7ecd20f78cac09969a9d3ee26224c9976ef3
Бек Аскер <2004bek.asker@mail.ru>
2024-10-17 04:37:39 +0600

    feat: desktop destination mars page

---
+++ script.js
@@ -0,0 +1,29 @@
+const route = (event) => {
+  event  = event || window.event
+  event.preventDefault()
+  window.history.pushState({}, '', event.target.href)
+  handleLocation()
+}
+
+const routes = {
+  '/': '/home.html',
+  '/destination': '/pages/destination/destination-moon.html'
+}
+
+const handleLocation = async () => {
+  const path = window.location.pathname
+  const route = routes[path]
+  const html = await fetch(route).then((data) => data.text())
+  document.getElementById('main').innerHTML = html
+  if (path !== '/'){
+    document.body.style.backgroundImage = "url('/assets/destination/background-destination-desktop.jpg')"
+  }
+  else {
+    document.body.style.backgroundImage = "url(assets/home/background-home-desktop.jpg)"
+  }
+}
+
+window.onpopstate = handleLocation
+window.route = route
+
+handleLocation()
