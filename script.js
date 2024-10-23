9045e92ebcd2f9ac3dc5581fddd7405ab0ee008d
Бек Аскер <2004bek.asker@mail.ru>
2024-10-23 18:48:45 +0600

    feat: destination page routing

--- script.js
+++ script.js
@@ -1,44 +1,57 @@
 const route = (event) => {
-  event  = event || window.event
+  event = event || window.event
   event.preventDefault()
-  window.history.pushState({}, '', event.target.href)
+  window.history.pushState({}, "", event.target.href)
   handleLocation()
 }
 
-// const routes = {
-//   '/': '/pages/home.html',
-//   '/destination': '/pages/destination/destination-moon.html'
-// }
-
 const routes = {
-  '/': '/pages/destination/destination-moon.html'
+  "/": "/pages/home.html",
+  "/destination-moon": "/pages/destination/destination-moon.html",
+  "/destination-mars": "/pages/destination/destination-mars.html",
+  "/destination-europa": "/pages/destination/destination-europa.html",
+  "/destination-titan": "/pages/destination/destination-titan.html",
 }
+
+// const routes = {
+//   '/': '/pages/destination/destination-moon.html'
+// }
 
 const handleLocation = async () => {
   const path = window.location.pathname
   const route = routes[path]
   const html = await fetch(route).then((data) => data.text())
-  document.getElementById('main').innerHTML = html
-  if (path == '/destination'){
+  document.getElementById("main").innerHTML = html
+  if (path == "/") {
     if (window.innerWidth > 1024) {
-      document.body.style.backgroundImage = "url(assets/home/background-home-desktop.jpg)"
-    }
-    else if (window.innerWidth > 480 && window.innerWidth < 1024) {
-      document.body.style.backgroundImage = "url(assets/home/background-home-tablet.jpg)"
-    }
-    else if (window.innerWidth < 481) {
-      document.body.style.backgroundImage = "url(assets/home/background-home-mobile.jpg)"
+      document.body.style.backgroundImage =
+        "url(assets/home/background-home-desktop.jpg)"
+    } else if (window.innerWidth > 480 && window.innerWidth < 1024) {
+      document.body.style.backgroundImage =
+        "url(assets/home/background-home-tablet.jpg)"
+    } else if (window.innerWidth < 481) {
+      document.body.style.backgroundImage =
+        "url(assets/home/background-home-mobile.jpg)"
     }
   }
-  else if (path == '/') {
+   
+  else if 
+  (
+    path == "/destination-moon" ||
+    path == "/destination-mars" ||
+    path == "/destination-europa" ||
+    path == "/destination-titan"
+  ) 
+  {
     if (window.innerWidth > 1024) {
-      document.body.style.backgroundImage = "url('/assets/destination/background-destination-desktop.jpg')"
-    }
-    else if (window.innerWidth > 480 && window.innerWidth < 1024) {
-      document.body.style.backgroundImage = "url('/assets/destination/background-destination-tablet.jpg')"
-    }
-    else if (window.innerWidth < 481) {
-      document.body.style.backgroundImage = "url('/assets/destination/background-destination-mobile.jpg')"
+      document.body.style.backgroundImage =
+        "url('/assets/destination/background-destination-desktop.jpg')"
+    } else if (window.innerWidth > 480 && window.innerWidth < 1024) {
+      document.body.style.backgroundImage =
+        "url('/assets/destination/background-destination-tablet.jpg')"
+    } else if (window.innerWidth < 481) {
+      document.body.style.backgroundImage =
+        "url('/assets/destination/background-destination-mobile.jpg')"
     }
   }
 }
@@ -48,23 +61,21 @@ window.route = route
 
 handleLocation()
 
+var isNavbarOpened = false
 
-
-var isNavbarOpened = false 
-
-const navbar = document.getElementById('navbar')
-const openNavberIcon = document.getElementById('open-navbar')
+const navbar = document.getElementById("navbar")
+const openNavberIcon = document.getElementById("open-navbar")
 
 const openNavbar = () => {
   console.log(isNavbarOpened)
-  if (isNavbarOpened == false){
-    navbar.style.display = 'grid'
+  if (isNavbarOpened == false) {
+    navbar.style.display = "grid"
     openNavberIcon.style.backgroundImage = "url(assets/shared/icon-close.svg)"
     isNavbarOpened = true
-  }
-  else {
-    navbar.style.display = 'none'
-    openNavberIcon.style.backgroundImage = "url(assets/shared/icon-hamburger.svg)"
+  } else {
+    navbar.style.display = "none"
+    openNavberIcon.style.backgroundImage =
+      "url(assets/shared/icon-hamburger.svg)"
     isNavbarOpened = false
   }
 }
